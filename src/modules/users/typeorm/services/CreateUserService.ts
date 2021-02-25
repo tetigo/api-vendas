11afd68860876170f2d0918049988eafc9fe699f
Tiago Mendes <tiago.mendes@dsbsolucoes.com.br>
2021-02-25 17:47:47 -0300

    users repository

---
+++ src/modules/users/typeorm/services/CreateUserService.ts
@@ -0,0 +1,25 @@
+import AppError from "@shared/errors/AppError"
+import { getCustomRepository } from "typeorm"
+import User from "../entitties/users"
+import UsersRepository from "../repositories/UsersRepository"
+
+interface IRequest{
+  name: string,
+  email: string
+}
+
+class CreateUserService{
+  public async execute({name, email}:IRequest): Promise<User | undefined>{
+    const usersRepository = getCustomRepository(UsersRepository)
+    let user = await usersRepository.findByEmail(email)
+    if(user){
+      throw new AppError('There is already one user with this email.')
+    }
+    user = usersRepository.create({
+      name, email
+    })
+    usersRepository.save(user)
+    return user
+  }
+}
+export default CreateUserService
